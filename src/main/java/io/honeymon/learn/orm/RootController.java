9f1f5731f64bf4cae50dbcc3e366f8108cd1674b
ihoneymon <ihoneymon@gmail.com>
2016-03-02 15:15:19 +0900

    Thymeleaf layout 기본구성, webjars 구성

--- src/main/java/io/honeymon/learn/orm/RootController.java
+++ src/main/java/io/honeymon/learn/orm/RootController.java
@@ -1,13 +1,15 @@
 package io.honeymon.learn.orm;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class RootController {
 
     @RequestMapping("/")
-    public String viewIndex() {
+    public String viewIndex(Model model) {
+        model.addAttribute("greeting", "Hello, world!");
         return "index";
     }
 }
